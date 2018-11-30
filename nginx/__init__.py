5fd874774c63fdc4b8e49c86e364645ada98704d
Fantix King <fantix.king@gmail.com>
2018-11-30 19:02:08 +0800

    Add load_module hint

--- nginx/__init__.py
+++ nginx/__init__.py
@@ -12,4 +12,21 @@ try:
         NginxEventLoop, NginxEventLoopPolicy, Event,
     )
 except ImportError:
-    print('Cannot import nginx, should load in NGINX first')
+    import sys
+    from os.path import abspath, dirname
+    from importlib.util import find_spec
+    spec = find_spec('nginx._nginx')
+
+    if spec and ('' not in sys.path or
+                 abspath(dirname(spec.origin)) != abspath(dirname(__file__))):
+        print('Cannot import nginx, should load in nginx.conf first:')
+        print()
+        print(f'    load_module {spec.origin};')
+        print(f'    python_enabled on;')
+        print()
+    elif '' in sys.path:
+        sys.modules.pop('nginx')
+        sys.path.remove('')
+        __import__('nginx')
+    else:
+        print('Cannot import nginx, please check installation.')
