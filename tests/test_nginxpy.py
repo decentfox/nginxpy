36e3d4d200beabbc39bc04032ce5155426fc4d0c
Fantix King <fantix.king@gmail.com>
2018-12-01 20:10:56 +0800

    Test NGINX dynamic load module

--- tests/test_nginxpy.py
+++ tests/test_nginxpy.py
@@ -3,14 +3,28 @@
 
 """Tests for `nginxpy` package."""
 
-
-import os
+import re
+import subprocess
+import tempfile
 import unittest
+from os.path import exists, abspath
 
 import nginx
+
+CONF_OK = re.compile(r'configuration file ([^ ]+) test is successful')
 
 
 class TestNginxpy(unittest.TestCase):
     def test_extension(self):
         self.assertTrue(hasattr(nginx, 'spec'))
-        self.assertTrue(os.path.exists(nginx.spec.origin))
+        self.assertTrue(exists(nginx.spec.origin))
+
+        with tempfile.NamedTemporaryFile('w') as f:
+            f.write('error_log stderr;pid ')
+            with tempfile.NamedTemporaryFile() as pid:
+                f.write(abspath(pid.name))
+            f.write(';load_module ')
+            f.write(nginx.spec.origin)
+            f.write(';events {}')
+            f.flush()
+            subprocess.check_call(['nginx', '-c', abspath(f.name), '-t'])
